9faac56cee06bbb47eb137257caf8283d5c085b3
alexanderjuxoncobb <alexanderjuxoncobb@gmail.com>
2024-10-03 10:50:37 +0100

    Initial container for the calculator created.

---
+++ script.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
